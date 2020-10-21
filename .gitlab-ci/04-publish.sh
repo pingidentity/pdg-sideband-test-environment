b8da9eeaeaa926b6dbcb98942cc343c8c6bb4d97
Osha Fuangkasae <oshafuangkasae@pingidentity.com>
2020-10-21 00:43:13 -0500

    Push to GitHub main during publish

--- .gitlab-ci/04-publish.sh
+++ .gitlab-ci/04-publish.sh
@@ -3,6 +3,7 @@ set -euo pipefail
 
 echo "Pushing to GitHub..."
 git remote add github \
-  "https://${GITHUB_USER}:${GITHUB_ACCESS_TOKEN}@github.com/pingidentity/pdg-sideband-test-environment.git"
+  "https://${GITHUB_USER}:${GITHUB_ACCESS_TOKEN}@github.com/pingidentity/pdg-sideband-test-environment.git" >/dev/null 2>&1 || echo "github remote already exists. Skipping add."
+git push github main
 git push github "$CI_COMMIT_TAG"
 exit 0
